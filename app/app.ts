f794d20f7917b1e2940f654d49cfadcc6f541e24
Alex Dragoi <dragoi.alex@gmail.com>
2022-01-08 10:44:12 +0200

    configure app

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,7 @@
 function startGame() {
+    // starting a new game
     var messagesElement = document.getElementById("messages");
-    messagesElement.innerText = "Welcome to MultiMath! Starting a new game...";
+    messagesElement!.innerText = "Welcome to MultiMath! Starting a new game...";
 }
 
-document.getElementById("startGame").addEventListener("click", startGame);
+document.getElementById("startGame")!.addEventListener("click", startGame);
